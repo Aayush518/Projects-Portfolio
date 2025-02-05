0872e2c0167055eeddf37a97db236604273787d9
Aayush Adhikari <adhikariaayush37@gmail.com>
2025-02-05 11:41:15 +0545

    Update: make changes in about.astro section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
-import { defineConfig } from 'astro';
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  integrations: [react()],
+  integrations: [tailwind(), react()],
 });
